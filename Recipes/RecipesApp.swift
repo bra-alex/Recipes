92f9f4b44c59e9e0e69d1e72deb15c013f419da5
Alexander Asiamah Boakye <aalexanderkwaku@yahoo.co.uk>
2022-11-11 20:57:23 +0000

    Initial Commit

---
+++ Recipes/RecipesApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipesApp.swift
+//  Recipes
+//
+//  Created by Don Bouncy on 11/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct RecipesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
